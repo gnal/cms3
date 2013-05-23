0f0c36b357f7a52e0d17ced709e99327186c6b9c
Aleksis Joubert <qwertypianox@gmail.com>
2013-05-23 15:41:24 -0400

    fix

--- Admin/BlockAdmin.php
+++ Admin/BlockAdmin.php
@@ -22,6 +22,7 @@ class BlockAdmin extends Admin
     public function buildGrid(GridBuilder $builder)
     {
         $builder
+            ->add('published', 'boolean')
             ->add('name')
             ->add('', 'action')
         ;
